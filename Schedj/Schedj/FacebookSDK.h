c1e437e0b66c5b2546c57b1aefb459bf71c3bbfb
Nimarit Walia <nimarit@berkeley.edu>
2016-04-29 18:22:33 -0700

    error check

---
+++ Schedj/Schedj/FacebookSDK.h
@@ -0,0 +1,15 @@
+//
+//  FacebookSDK.h
+//  Schedj
+//
+//  Created by Nimarit Walia on 4/23/16.
+//  Copyright © 2016 Nimarit Walia. All rights reserved.
+//
+
+#ifndef FacebookSDK_h
+#define FacebookSDK_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* FacebookSDK_h */
---
+++ Schedj/Schedj/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  Schedj
+//
+//  Created by Nimarit Walia on 4/22/16.
+//  Copyright © 2016 Nimarit Walia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
